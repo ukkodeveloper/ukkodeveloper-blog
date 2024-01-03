e8aef83db299c0f1ee484e8476d894fa78e43669
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-03 17:12:40 +0900

    feat: main page title style
    (cherry picked from commit 92bf981d77daead0ee58d2f13fba16a43ff91003)

--- app/Main.tsx
+++ app/Main.tsx
@@ -5,6 +5,7 @@ import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
 import type { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog } from 'contentlayer/generated'
+import Title from '@/components/Title'
 
 const MAX_DISPLAY = 5
 
@@ -15,15 +16,9 @@ interface Props {
 export default function Home({ posts }: Props) {
   return (
     <>
+      <Title title={'Series'} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
-        </div>
+        <Title title={'Articles'} />
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
