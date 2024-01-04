724cad8a33282061f3674ff2743b6aefb60709b4
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-04 12:13:06 +0900

    feat: remove discuss link to twitter or github page
    (cherry picked from commit 1fc8076d3f44cc3d3225c2ebd03d797564dccb0c)

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -11,6 +11,7 @@ import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 import TOCInline from 'pliny/ui/TOCInline'
 import type { Toc } from 'pliny/mdx-plugins'
+import TOCInline_default from 'pliny/ui/TOCInline'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
@@ -96,13 +97,6 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
-              <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(path)} rel="nofollow">
-                  Discuss on Twitter
-                </Link>
-                {` • `}
-                <Link href={editUrl(filePath)}>View on GitHub</Link>
-              </div>
               {siteMetadata.comments && (
                 <div
                   className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300"
