bfa2e2bb4c98e60a0b399820c6e019e23d268585
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-04 13:44:44 +0900

    fix: title component margin padding
    (cherry picked from commit 3c2d1b40bbe86b6282669ffc8a878793b9519fb6)

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,7 +4,7 @@ interface TitleProps {
 
 const Title = ({ title }: TitleProps) => {
   return (
-    <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+    <div className="space-y-2 py-3">
       <h1 className="relative text-2xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-9 md:text-4xl md:leading-10 ">
         <span className="relative inline-block">
           <span className="rotate-4 absolute left-3 -z-0  w-full rotate-3 rounded-bl-[45%] rounded-br-[24%] rounded-tl-[32%] rounded-tr-[25%] bg-purple-300 p-5 opacity-75 sm:p-6 md:p-7"></span>
