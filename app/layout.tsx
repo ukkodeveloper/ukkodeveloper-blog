bc57fd72f908ce2af84946137ff0b0e3bd36a001
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-05 20:01:19 +0900

    chore: erase older font settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
-
-import { Space_Grotesk } from 'next/font/google'
 import localFont from 'next/font/local'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
@@ -11,12 +9,6 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-})
 
 const myFont = localFont({
   src: './fonts/GmarketSansTTFMedium.ttf',
